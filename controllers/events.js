92400e119d67763c705d37d48a6eeeddab6bc4cb
Pablo Chuquimia <i.pouk.19@gmail.com>
2025-05-14 14:24:04 -0400

    auth controller updated

--- controllers/events.js
+++ controllers/events.js
@@ -93,7 +93,7 @@ const eliminarEvento = async (req, res = response) => {
     if (evento.user.toString() !== uid) {
       return res.status(401).json({
         ok: false,
-        msg: "No tiene privilegio de editar este evento",
+        msg: "No tiene privilegio de eliminar este evento",
       });
     }
 
